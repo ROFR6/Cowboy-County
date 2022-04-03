adad443527356cb8ecfd8566ad78a1ffcd53a3bb
RossCowboyCounty <rossfriar2201@gmail.com>
2022-04-03 13:00:35 -0400

    update

---
+++ html/staking.php
@@ -0,0 +1,15 @@
+<?php
+
+    $conn = mysqli_connect('localhost',"ross",'Irish18!Roster9!','cc_tokens');
+    
+    if(!$conn){
+        echo 'Connection error: ' . mysqli_connect_error();
+        
+    }
+
+?>
+
+<!DOCTYPE html>
+<html>
+    
+</html>
